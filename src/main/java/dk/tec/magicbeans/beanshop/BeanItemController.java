c685060420ee273844e228a05188b4368189e273
PhilipData <88774940+PhilipData@users.noreply.github.com>
2024-09-11 14:13:00 +0200

    got my database and api up and running time to either implement my delete method or go to front end and work on recieving data

--- src/main/java/dk/tec/magicbeans/beanshop/BeanItemController.java
+++ src/main/java/dk/tec/magicbeans/beanshop/BeanItemController.java
@@ -2,6 +2,8 @@ package dk.tec.magicbeans.beanshop;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/beanshop")
 public class BeanItemController {
@@ -24,5 +26,23 @@ public class BeanItemController {
         return repository.findById(id).get();
     }
 
+    @GetMapping()
+    List<BeanItem> getAll(){
+        return repository.findAll();
+    }
+
+    @PutMapping()
+    void update(@RequestBody BeanItem beanItem){
+        repository.save(beanItem);
+    }
+
+
+    /*
+    // ved ikke hvilken returtype jeg skal anvende her tænker int men så mangler jeg selve objektet
+    @DeleteMapping("/{id}")
+     BeanItem (@PathVariable int id){
+        return repository.deleteById(id);
+    }
+     */
 
 }
